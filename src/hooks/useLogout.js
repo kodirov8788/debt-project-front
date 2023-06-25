a9a1ea9faa54491af6b843444564546625795f32
kodirov8788 <kodirov8788@gmail.com>
2023-06-25 12:30:30 +0500

    sa

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -3,6 +3,7 @@ import { useAuthContext } from './useAuthContext'
 export const useLogout = () => {
   const { dispatch } = useAuthContext()
 
+
   const logout = () => {
     // remove user from storage
     localStorage.removeItem('user')
