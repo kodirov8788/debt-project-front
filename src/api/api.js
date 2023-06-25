27dc5437dd4336f37e71ae4ec528a9893ff96305
kodirov8788 <kodirov8788@gmail.com>
2023-06-25 13:07:16 +0500

    changed lot

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 
 // https://debt-project.onrender.com/
-const instance = axios.create({
-    baseURL: 'http://localhost:5000'
-});
 // const instance = axios.create({
-//     baseURL: 'https://debt-project.onrender.com'
+//     baseURL: 'http://localhost:5000'
 // });
+const instance = axios.create({
+    baseURL: 'https://debt-project.onrender.com'
+});
 
 export default instance
 
